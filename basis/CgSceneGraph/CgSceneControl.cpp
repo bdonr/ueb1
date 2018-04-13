88651ec178442f6d9dd43eb9694e59dcb32d3391
bdonr <joker_tn@web.de>
2018-04-13 20:14:03 +0200

    struk

---
+++ basis/CgSceneGraph/CgSceneControl.cpp
@@ -0,0 +1,82 @@
+#include "CgSceneControl.h"
+#include "CgBase/CgEnums.h"
+#include "CgEvents/CgMouseEvent.h"
+#include "CgEvents/CgKeyEvent.h"
+#include "CgEvents/CgWindowResizeEvent.h"
+#include "CgBase/CgBaseRenderer.h"
+#include "CgExampleTriangle.h"
+#include <iostream>
+#include <glm/gtc/matrix_transform.hpp>
+
+CgSceneControl::CgSceneControl()
+{
+     m_triangle=new CgExampleTriangle(21);
+     m_current_transformation=glm::mat4(1.);
+     m_proj_matrix= glm::mat4x4(glm::vec4(1.792591, 0.0, 0.0, 0.0), glm::vec4(0.0, 1.792591, 0.0, 0.0), glm::vec4(0.0, 0.0, -1.0002, -1.0), glm::vec4(0.0, 0.0, -0.020002, 0.0));
+}
+
+
+CgSceneControl::~CgSceneControl()
+{
+     delete m_triangle;
+}
+
+void CgSceneControl::setRenderer(CgBaseRenderer* r)
+{
+    m_renderer=r;
+    m_renderer->setSceneControl(this);
+
+    m_renderer->init(m_triangle);
+}
+
+
+void CgSceneControl::renderObjects()
+{
+    m_renderer->setProjectionMatrix(m_proj_matrix);
+    m_renderer->setLookAtMatrix(glm::mat4x4(glm::vec4(1.0, 0.0, 0.0, 0.0), glm::vec4(0.0, 1.0, 0.0, 0.0), glm::vec4(0.0, 0.0, 1.0, -1.0), glm::vec4(0.0, 0.0, -1.0, 1.0)));
+
+    m_renderer->render(m_triangle,m_current_transformation);
+
+}
+
+
+
+void CgSceneControl::handleEvent(CgBaseEvent* e)
+{
+    // die Enums sind so gebaut, dass man alle Arten von MausEvents über CgEvent::CgMouseEvent abprüfen kann,
+    // siehe dazu die CgEvent enums im CgEnums.h
+
+
+    if(e->getType() & Cg::CgMouseEvent)
+    {
+        CgMouseEvent* ev = (CgMouseEvent*)e;
+        std::cout << *ev << std::endl;
+
+         // hier kommt jetzt die Abarbeitung des Events hin...
+    }
+
+    // die Enums sind so gebaut, dass man alle Arten von KeyEvents über CgEvent::CgKeyEvent abprüfen kann,
+    // siehe dazu die CgEvent enums im CgEnums.h
+    // momentan werden nur KeyPressEvents gefangen.
+
+    if(e->getType() & Cg::CgKeyEvent)
+    {
+        CgKeyEvent* ev = (CgKeyEvent*)e;
+        std::cout << *ev <<std::endl;
+
+        // hier kommt jetzt die Abarbeitung des Events hin...
+    }
+
+    if(e->getType() & Cg::WindowResizeEvent)
+    {
+         CgWindowResizeEvent* ev = (CgWindowResizeEvent*)e;
+         std::cout << *ev <<std::endl;
+         m_proj_matrix=glm::perspective(45.0f, (float)(ev->w()) / ev->h(), 0.01f, 100.0f);
+    }
+
+
+    // an der Stelle an der ein Event abgearbeitet ist wird es auch gelöscht.
+    delete e;
+
+
+}
---
+++ basis/CgSceneGraph/CgSceneControl.h
@@ -0,0 +1,32 @@
+#ifndef CGSCENECONTROL_H
+#define CGSCENECONTROL_H
+
+#include "CgBase/CgObserver.h"
+#include "CgBase/CgBaseSceneControl.h"
+#include <glm/glm.hpp>
+
+class CgBaseEvent;
+class CgBaseRenderer;
+class CgExampleTriangle;
+
+class CgSceneControl : public CgObserver, public CgBaseSceneControl
+{
+public:
+    CgSceneControl();
+    ~CgSceneControl();
+    void handleEvent(CgBaseEvent* e);
+    void setRenderer(CgBaseRenderer* r);
+
+    void renderObjects();
+
+private:
+
+    CgBaseRenderer* m_renderer;
+
+    CgExampleTriangle* m_triangle;
+    glm::mat4 m_current_transformation;
+    glm::mat4 m_proj_matrix;
+
+};
+
+#endif // CGSCENECONTROL_H
