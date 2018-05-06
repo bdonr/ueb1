e0f57fbb5c903d1000527892b28f97e754ea822f
bdonr <joker_tn@web.de>
2018-05-06 07:44:57 +0200

    sdasd

--- ueb2/CgSceneGraph/CgSceneControl.cpp
+++ ueb2/CgSceneGraph/CgSceneControl.cpp
@@ -31,10 +31,18 @@ CgSceneControl::CgSceneControl() {
     poly = MeshFactory::createRotationKoerper(1);
     m_triangle.push_back(MeshFactory::createKegel(0,.0,0));
     dreiecke=NULL;
+    glm::vec3 b2 = glm::vec3(9.,1.,1.);
+    glm::vec3 b1 = glm::vec3(0,0,0);
+    std::vector<glm::vec3> st;
+    st.push_back(b1);
+    st.push_back(b2);
+
+    rotaAchse = MeshFactory::createMyPolyline(glm::vec3(220,9,200),st);
     //resetRender(100);
 
-
     m_current_transformation = glm::mat4(1.);
+
+    glm::mat4x4 old=m_current_transformation;
     m_proj_matrix = glm::mat4x4(glm::vec4(1.792591, 0.0, 0.0, 0.0), glm::vec4(0.0, 1.792591, 0.0, 0.0),
                                 glm::vec4(0.0, 0.0, -1.0002, -1.0), glm::vec4(0.0, 0.0, -0.020002, 0.0));
     /*
@@ -74,6 +82,10 @@ void CgSceneControl::setRenderer(CgBaseRenderer *r) {
             }
 
         }
+        if(rotaAchse){
+            std::cout<<"huhu"<<std::endl;
+            m_renderer->init(rotaAchse);
+        }
     }
 
 
@@ -105,6 +117,9 @@ void CgSceneControl::renderObjects() {
                             glm::vec4(0.0, 0.0, -1.0, 1.0)));
     for(int i = 0; i<=koordinatensystem->getPolylines().size()-1;i++){
         m_renderer->render(koordinatensystem->getPolylines().at(i),m_current_transformation);
+    }
+    if(rotaAchse!=NULL){
+        m_renderer->render(rotaAchse,m_current_transformation);
     }
     if(poly){
         if(!poly->getKeisVec().empty())
@@ -153,6 +168,38 @@ void CgSceneControl::reset(){
 }
 int CgSceneControl::getChanged(){
     return changed;
+}
+
+glm::mat4x4 CgSceneControl::rotationX(int winkelx)
+{
+    return glm::mat4x4(glm::vec4(1,0,0,0),
+                       glm::vec4(0,glm::cos(glm::radians(winkelx*1.0)),(-1)*glm::sin(glm::radians(winkelx*1.0)),0),
+                       glm::vec4(0,glm::sin(glm::radians(winkelx*1.0)),glm::cos(glm::radians(winkelx*1.0)),0),
+                       glm::vec4(0,0,0,1));
+}
+
+glm::mat4x4 CgSceneControl::rotationY(int winkely)
+{
+    return glm::mat4x4(glm::vec4(glm::cos(glm::radians(winkely*1.0)),0,glm::sin(glm::radians(winkely*1.0)),0),
+                       glm::vec4(0,1,0,0),
+                       glm::vec4((-1)*glm::sin(glm::radians(winkely*1.0)),0,glm::cos(glm::radians(winkely*1.0)),0),
+                       glm::vec4(0,0,0,1));
+}
+
+glm::mat4x4 CgSceneControl::rotationZ(int winkelz)
+{
+    return glm::mat4x4(glm::vec4(glm::cos(glm::radians(winkelz*1.0)),(-1)*glm::sin(glm::radians(winkelz*1.0)),0,0),
+                       glm::vec4(glm::sin(glm::radians(winkelz*1.0)),glm::cos(glm::radians(winkelz*1.0)),0,0),
+                       glm::vec4(0,0,1,0),
+                       glm::vec4(0,0,0,1));
+}
+
+glm::mat4x4 CgSceneControl::transform(glm::vec3 k)
+{
+    return glm::mat4x4(glm::vec4(s,0,0,k.x),
+                glm::vec4(0,s,0,k.y),
+                glm::vec4(0,0,s,k.z),
+                glm::vec4(0,0,0,s));
 }
 
 void CgSceneControl::handleEvent(CgBaseEvent *e) {
@@ -268,6 +315,64 @@ void CgSceneControl::handleEvent(CgBaseEvent *e) {
         if(s<0.01){
             s=0.01;
         }else{
+            s=s-0.2;
+        }
+        m_current_transformation=m_current_transformation*(glm::mat4x4(glm::vec4(s,0,0,0),
+                                                                       glm::vec4(0,s,0,0),
+                                                                       glm::vec4(0,0,s,0),
+                                                                       glm::vec4(0,0,0,1)));
+    }
+    if(((CgKeyEvent*)e)->key()==Cg::Key_Plus){
+        std::cout<<"§§"<<std::endl;
+        if(s>0.9){
+            s=0.9;
+        }else{
+            s=s*1.2;
+        }
+
+        m_current_transformation=m_current_transformation*(glm::mat4x4(glm::vec4(s,0,0,0),
+                                                                       glm::vec4(0,s,0,0),
+                                                                       glm::vec4(0,0,s,0),
+                                                                       glm::vec4(0,0,0,1)));
+
+    }
+
+    if(((CgKeyEvent*)e)->key()==Cg::Key_Y){
+        m_current_transformation=m_current_transformation*rotationY(y);
+        if(y==3){
+            y=0;
+        }
+        else{
+            y=y+.5;
+        }
+
+    }
+    if(((CgKeyEvent*)e)->key()==Cg::Key_X){
+        m_current_transformation=m_current_transformation*rotationX(x);
+        if(x==3){
+            x=0;
+        }
+        else{
+            x=x+0.5;
+        }
+
+    }
+
+    if(((CgKeyEvent*)e)->key()==Cg::Key_Z){
+        m_current_transformation=m_current_transformation*rotationZ(z);
+        if(z==3){
+            z=0;
+        }
+        else{
+            z=z+0.5;
+        }
+
+    }
+
+    if(((CgKeyEvent*)e)->key()==Cg::Key_Minus){
+        if(s<0.01){
+            s=0.01;
+        }else{
             s=s-0.02;
         }
         m_current_transformation=(glm::mat4x4(glm::vec4(s,0,0,0),
@@ -289,54 +394,22 @@ void CgSceneControl::handleEvent(CgBaseEvent *e) {
 
     }
 
-    if(((CgKeyEvent*)e)->key()==Cg::Key_Y){
-        m_current_transformation=m_current_transformation*(glm::mat4x4(glm::vec4(glm::cos(glm::radians(y)),0,glm::sin(glm::radians(y)),0),
-                                                                       glm::vec4(0,1,0,0),
-                                                                       glm::vec4((-1)*glm::sin(glm::radians(y)),0,glm::cos(glm::radians(y)),0),
-                                                                       glm::vec4(0,0,0,1)));
-        if(y==3){
-            y=0;
-        }
-        else{
-            y=y+.5;
-        }
-
-    }
-
-    if(((CgKeyEvent*)e)->key()==Cg::Key_X){
-        m_current_transformation=m_current_transformation*(glm::mat4x4(glm::vec4(1,0,0,0),
-                                                                       glm::vec4(0,glm::cos(glm::radians(x)),(-1)*glm::sin(glm::radians(x)),0),
-                                                                       glm::vec4(0,glm::sin(glm::radians(x)),glm::cos(glm::radians(x)),0),
-                                                                       glm::vec4(0,0,0,1)));
-        if(x==3){
-            x=0;
-        }
-        else{
-            x=x+0.5;
-        }
-
-    }
-
-    if(((CgKeyEvent*)e)->key()==Cg::Key_Z){
-        m_current_transformation=m_current_transformation*(glm::mat4x4(glm::vec4(glm::cos(glm::radians(z)),(-1)*glm::sin(glm::radians(z)),0,0),
-                                                                       glm::vec4(glm::sin(glm::radians(z)),glm::cos(glm::radians(z)),0,0),
-                                                                       glm::vec4(0,0,1,0),
-                                                                       glm::vec4(0,0,0,1)));
-        if(z==3){
-            z=0;
-        }
-        else{
-            z=z+0.5;
-        }
-
-    }
-
     if(e->getType()==Cg::CgChangeRota){
-        std::cout<<((bestersliderMoveEvent*)e)->getTraegerKlasse()->getIntvec().x<<std::endl;
-        std::cout<<((bestersliderMoveEvent*)e)->getTraegerKlasse()->getIntvec().y<<std::endl;
-        std::cout<<((bestersliderMoveEvent*)e)->getTraegerKlasse()->getIntvec().z<<std::endl;
+        bool k=false;
+        int x =((bestersliderMoveEvent*)e)->getTraegerKlasse()->getIntvec().x;
+        std::cout<<x<<"x"<<std::endl;
 
+
+        int y =((bestersliderMoveEvent*)e)->getTraegerKlasse()->getIntvec().y;
+        int z =((bestersliderMoveEvent*)e)->getTraegerKlasse()->getIntvec().z;
+        //glm::vec3 b3=glm::vec3((-1.)*b2.x,(-1.)*b2.y,(-1.)*b2.z);
+        //std::cout<<b3.x<<b3.y<<b3.z<<std::endl;
+        glm::mat4x4 bla=(rotationZ(z)*rotationY(y)*rotationX(x)*rotationZ(-1.*z)*rotationY(-1.*y));
+        m_current_transformation=old*bla;
+        x=true;
     }
+
+
     if(e->getType()==Cg::CgChangeWahl){
 
         if(objecte.empty()){
@@ -363,4 +436,16 @@ void CgSceneControl::handleEvent(CgBaseEvent *e) {
     delete e;
 
 }
+
+glm::mat4x4 CgSceneControl::allgemineRotation(int x, int y, int z){
+    float x1=x*1.;
+    float y1=y*1.;
+    float z1=z*1.0;
+}
+
+glm::mat4x4 rotationX(int x){
+
+}
+
+//glm::mat4x4 CgSceneControl::rotation(int x, int y, int z)
 
--- ueb2/CgSceneGraph/CgSceneControl.h
+++ ueb2/CgSceneGraph/CgSceneControl.h
@@ -25,7 +25,14 @@ public:
     void resetObject();
     void reset();
     int getChanged();
+    glm::mat4x4 rotationX(int winkelx);
+    glm::mat4x4 rotationY(int winkely);
+    glm::mat4x4 rotationZ(int winkelz);
+    glm::mat4x4 transform(glm::vec3 k);
+
+    glm::mat4x4 allgemineRotation(int x, int y, int z);
 private:
+    glm::mat4x4 old;
     std::vector <Dreiecke*> objecte;
     CgBaseRenderer* m_renderer;
     Dreiecke* dreiecke;
@@ -33,6 +40,7 @@ private:
     std::vector<CgBaseTriangleMesh*> m_triangle;
     glm::mat4 m_current_transformation;
     glm::mat4 m_proj_matrix;
+    MyPolyline* rotaAchse;
     float s;
     float x;
     float y;
@@ -41,6 +49,8 @@ private:
     std::vector<glm::vec3> dreickevertices;
     Koordinatensystem* koordinatensystem;
     std::vector<unsigned int> dreieckecords;
+
+
 
 
 
