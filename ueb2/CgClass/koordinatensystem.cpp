86281a6353721a174759c3e98603c93e559c860a
bdonr <joker_tn@web.de>
2018-06-09 23:45:53 +0200

    pfeil anfang

--- ueb2/CgClass/koordinatensystem.cpp
+++ ueb2/CgClass/koordinatensystem.cpp
@@ -35,23 +35,23 @@ glm::mat4x4 Koordinatensystem::rotationX()
 
 
     return glm::mat4x4(glm::vec4(1,0,0,0),
-                       glm::vec4(0,glm::cos(glm::radians(180)),glm::sin(glm::radians(180)),0),
-                       glm::vec4(0,(-1)*glm::sin(glm::radians(180)),glm::cos(glm::radians(180)),0),
+                       glm::vec4(0,glm::cos(glm::radians(90.0)),glm::sin(glm::radians(90.0)),0),
+                       glm::vec4(0,(-1)*glm::sin(glm::radians(90.0)),glm::cos(glm::radians(90.0)),0),
                        glm::vec4(0,0,0,1));
 }
 
 glm::mat4x4 Koordinatensystem::rotationY()
 {
-    return glm::mat4x4(glm::vec4(glm::cos(glm::radians(180)),0,(-1)*glm::sin(glm::radians(180)),0),
+    return glm::mat4x4(glm::vec4(glm::cos(glm::radians(90.0)),0,(-1)*glm::sin(glm::radians(90.0)),0),
                        glm::vec4(0,1,0,0),
-                       glm::vec4(glm::sin(glm::radians(180)),0,glm::cos(glm::radians(180)),0),
+                       glm::vec4(glm::sin(glm::radians(90.0)),0,glm::cos(glm::radians(90.0)),0),
                        glm::vec4(0,0,0,1));
 }
 
 glm::mat4x4 Koordinatensystem::rotationZ()
 {
-    return glm::mat4x4(glm::vec4(glm::cos(glm::radians(180)),glm::sin(glm::radians(180)),0,0),
-                       glm::vec4((-1)*glm::sin(glm::radians(180)),glm::cos(glm::radians(180)),0,0),
+    return glm::mat4x4(glm::vec4(glm::cos(glm::radians(90.0)),glm::sin(glm::radians(90.0)),0,0),
+                       glm::vec4((-1)*glm::sin(glm::radians(90.0)),glm::cos(glm::radians(90.0)),0,0),
                        glm::vec4(0,0,1,0),
                        glm::vec4(0,0,0,1));
 }
